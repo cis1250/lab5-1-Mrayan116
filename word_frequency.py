629f94505d76004421c4ce9f3acd265c0b57dbbe
Mrayan116 <mrayan@uoguelph.ca>
2025-10-20 12:53:08 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -24,3 +24,39 @@ def is_sentence(text):
         return False
 
     return True
+
+
+
+def get_sentence():
+    while True:
+        sentence = input("Enter a sentence: ")  # ask the user to enter a full sentence
+        if len(sentence) > 1 and sentence[0].isupper() and sentence[-1] in ".!?":  # checks if it starts with a capital and ends with punctuation
+            return sentence  # returns the valid sentence when conditions are met
+        print("Invalid sentence. It must start with a capital letter and end with a period, question mark, or exclamation mark.")  # shows error message for invalid input
+________________________________________________________________________________________________________________________________________
+
+def calculate_frequencies(sentence):
+    sentence_list = sentence.split()  # splits the sentence into individual words and stores them in a list
+    words = []  
+    counts = []  # list to hold how many times each word appears, list to hold unique words
+
+    for w in sentence_list:  # loops through each word in the list
+        w = w.strip(".,!?").lower()  # removes punctuation and converts to lowercase for accurate counting
+        if w in words:  # checks if the word already exists in the list,  finds where the word is stored in the list
+            index = words.index(w) 
+            counts[index] += 1  # increases the count for that specific word
+        else:
+            words.append(w)  # adds the new word to the words list, starts its count at one since it appeared for the first time, returns both lists so they can be printed later
+            counts.append(1)  
+    return words, counts 
+
+def print_frequencies(words, frequencies):
+    print("\nWord frequencies:")  # prints header for clarity, loops through each index of the words list
+    for i in range(len(words)):  
+        print(words[i] + ":", frequencies[i])  # prints word followed by how many times it appeared
+
+def main():
+    sentence = get_sentence()  # get and validate sentence from the user
+    words, frequencies = calculate_frequencies(sentence)  
+    print_frequencies(words, frequencies)  # display the results neatly
+main()  
